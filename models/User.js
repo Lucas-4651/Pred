a1afa179a2112d3a717459879cb714666cfea9b5
root <root@localhost.localdomain>
2025-06-30 18:23:28 +0000

    first commit

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// Hook pour hasher le mot de passe avant la création
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
+// Méthode pour valider le mot de passe
+User.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = User;
